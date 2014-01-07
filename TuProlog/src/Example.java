b73cee317e21b8ed06236d236a3aa0dd604b3b3f
Felix Wackernagel <Felix Wackernagel@141.76.188.189>
2014-01-07 10:07:57 +0100

    Add documents and comments to example.

--- TuProlog/src/Example.java
+++ TuProlog/src/Example.java
@@ -5,33 +5,58 @@ import alice.tuprolog.SolveInfo;
 import alice.tuprolog.Theory;
 import alice.tuprolog.Var;
 
-public class Example {
-	public static void main( String[] args ) {
-		try {
-			Prolog engine = new Prolog();
+public class Example
+{
+	public static void main( String[] args )
+	{
+		try
+		{
+			// load prolog data
 			Theory theory = new Theory( Example.class.getClassLoader().getResourceAsStream( "database_example.txt" ) );
+			
+			// combine prolog with our data
+			Prolog engine = new Prolog();
 			engine.setTheory(theory);
-			SolveInfo solution = engine.solve("me(1569691,X).");
+			
+			// search X in our me's (module element) with student id 1569691
+			SolveInfo solution = engine.solve( "me(1569691,X)." );
 
-			while( solution.isSuccess() ) {
-				if( solution.getBindingVars().size() < 1 ) {
-					System.out.println(solution.toString());
-				} else {
+			// iterate over all found solutions
+			while( solution.isSuccess() )
+			{
+				if( solution.getBindingVars().size() < 1 )
+				{
+					// found no binding for result variable X
+					System.out.println( solution.toString() );
+				}
+				else 
+				{
+					// found binding for X
 					@SuppressWarnings( "rawtypes" )
 					Iterator it = solution.getBindingVars().iterator();
-					while( it.hasNext() ) {
+					while( it.hasNext() )
+					{
+						// print solution with variable and his binding value
 						Var type = (Var) it.next();
-						System.out.println(type.getName() + ": " + solution.getTerm(type.getName()));
+						System.out.println( type.getName() + ": " + solution.getTerm( type.getName() ) );
 					}
 				}
-				if( engine.hasOpenAlternatives() ) {
+				
+				if( engine.hasOpenAlternatives() )
+				{
+					// solve next alternative
 					solution = engine.solveNext();
-				} else {
+				}
+				else
+				{
+					// stop program
 					break;
 				}
 			}
-		} catch( Exception e ) {
-			e.printStackTrace();
+		} catch( Exception e )
+		{
+			System.out.println("tuProlog exception:" + e.getMessage());
 		}
 	}
+
 }
